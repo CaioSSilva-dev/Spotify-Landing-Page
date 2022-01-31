8e16bc271e10fb210d668ab2e8d02f861fb721cc
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-01-31 12:49:14 -0300

    bbbbbbb

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ function verificaLogin(){
     const email = document.querySelector('.email').value;
     const senha = document.querySelector('.senha').value;
     if(email == "admin" && senha == "admin"){
-            alert('Sucesso')
             location.href = "http://caiossilva.github.io/Spotify-Web-Player/"
         }else{
                 alert("Erro! Dados Incorretos")
             }
         }
+function esqueciDados(){
+    alert("(Email: admin) (Senha: admin)  Por Favor, guarde seus dados!")
+}
